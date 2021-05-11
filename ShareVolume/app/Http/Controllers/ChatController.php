cfc2e542501278109b469402ffd632ecb081a4cd
guillebarroso <guillebarroso98@gmail.com>
2021-05-11 10:31:31 +0200

    second commit

--- ShareVolume/app/Http/Controllers/ChatController.php
+++ ShareVolume/app/Http/Controllers/ChatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\MessageSent;
 use App\Models\Chat;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -11,33 +12,42 @@ class ChatController extends Controller
     public function sendMessage(Request $request)
     {
 
-        $info_mensaje =  Chat::create([
+//        $info_mensaje =  Chat::create([
+//            'sender_user_id' => $request->input('sender_user_id'),
+//            'reciever_user_id' => $request->input('reciever_user_id'),
+//            'message' => $request->input('message'),
+//        ]);
+
+        $sender_user_id = $request->input('sender_user_id');
+        $reciever_user_id = $request->input('reciever_user_id');
+        $message = $request->input('message');
+        event(new MessageSent($sender_user_id, $reciever_user_id, $message));
+
+        Chat::create([
             'sender_user_id' => $request->input('sender_user_id'),
             'reciever_user_id' => $request->input('reciever_user_id'),
             'message' => $request->input('message'),
         ]);
 
-        return response()->json($info_mensaje, 200);
+        return ["succes"=> true];
 
     }
 
     public function readMessages(Request $request)
     {
+        $sender_user_id = $request->input('sender_user_id');
+        $reciever_user_id = $request->input('reciever_user_id');
 
-        {
+        $chat = DB::table('chats')
+            ->where('sender_user_id', '=', $sender_user_id)
+            ->orWhere('sender_user_id','=',$reciever_user_id)
+            ->where('reciever_user_id', '=', $reciever_user_id)
+            ->orWhere('reciever_user_id','=',$sender_user_id)
+            ->select('message', 'sender_user_id', 'created_at')
+            ->orderBy('created_at', 'asc')
+            ->get();
 
-            $sender_user_id = $request->input('sender_user_id');
-            $reciever_user_id = $request->input('reciever_user_id');
-
-            $chat = DB::table('chats')
-                ->where('sender_user_id', '=', $sender_user_id)
-                ->where('reciever_user_id', '=', $reciever_user_id)
-                ->select('messages')
-                ->orderBy('created_at', 'desc')
-                ->get();
-
-            return response()->json($chat, 200);
-        }
+        return response()->json($chat, 200);
 
     }
 }
